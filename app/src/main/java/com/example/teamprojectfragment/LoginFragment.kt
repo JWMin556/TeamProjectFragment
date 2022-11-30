9f02493583afa5d820669fb25b989ea9c3371690
jyjiwoo <thswldn0317@kau.kr>
2022-11-30 09:01:07 +0900

    2nd

--- app/src/main/java/com/example/teamprojectfragment/LoginFragment.kt
+++ app/src/main/java/com/example/teamprojectfragment/LoginFragment.kt
@@ -1,12 +1,8 @@
 package com.example.teamprojectfragment
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.OpenableColumns
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +10,6 @@ import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import com.example.teamprojectfragment.databinding.FragmentLoginBinding
-import com.example.teamprojectfragment.databinding.FragmentMenuBinding
 import com.google.firebase.storage.FirebaseStorage
 import java.util.*
 
